872c7364588b57c74ae63773e24fbfc7c4814db1
IlCesa <giovanni.cesarano99@gmail.com>
2024-12-27 00:56:00 +0100

    fix

--- dnsway/resolver/iterative/__main__.py
+++ dnsway/resolver/iterative/__main__.py
@@ -1,10 +1,7 @@
-
-
-import socket
 from dnsway.dns.message.definition.resource_record import QCLASS_VALUES, QTYPE_VALUES
-from dnsway.dns.message.dns_message import DnsMessage
 from dnsway.dns.message.header import OPCODE_TYPE, QUERY_TYPE, RCODE_TYPE
-
+from dnsway.dns.message.dns_message import DnsMessage
+import socket
 
 dns_message = DnsMessage()
 dns_message.header.set_query_type(query_type=QUERY_TYPE.QUERY)
@@ -15,55 +12,52 @@ dns_message.header.set_rd(True)
 dns_message.header.set_ra(True)
 dns_message.header.qdcount = 1
 dns_message.header.ancount = 0
+dns_message.header.nscount = 0
 dns_message.header.arcount = 0
 
-dns_message.question.qname = "google.it"
-dns_message.question.qtype = QTYPE_VALUES.A
+
+dns_message.question.qname  = "www.github.com"
+dns_message.question.qtype  = QTYPE_VALUES.A
 dns_message.question.qclass = QCLASS_VALUES.IN
 
-print(dns_message.encode())
+encoded_dns_message = dns_message.encode()
 
 #dns_message.dump_message()
+server_address = ("8.8.8.8", 53)
+msg_byte_length = len(encoded_dns_message)
+print(f"byte size: {msg_byte_length}. encoded message: {encoded_dns_message}")
+if msg_byte_length <= 512:
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        print(f"Sending DNS query using UDP -> {server_address[0]}:{server_address[1]}...")
+        sock.sendto(dns_message.encode(), server_address)
+        data, addr = sock.recvfrom(4096)
+        print(f"Ricevuto {len(data)} byte da {addr}")
+        print(f"Contenuto: {data}")
+    except Exception as e:
+        print(f"Errore: {e}")
+    finally:
+        sock.close()
 
-server_address = ("1.1.1.1", 53)
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+if 1==1:
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-try:
-    print(f"Invio della query DNS  tramite UDP a {server_address[0]}:{server_address[1]}...")
-    sock.sendto(dns_message.encode(), server_address)
+    try:
+        print(f"Sending DNS query using TCP -> {server_address[0]}:{server_address[1]}...")
+        sock.connect(server_address)
+        msg = bytearray()
+        msg = msg + msg_byte_length.to_bytes(length=2, byteorder='big') + dns_message.encode()
 
-    # Ricevi i dati dal socket
-    data, addr = sock.recvfrom(4096)  # Leggi fino a 4096 byte
-    print(f"Ricevuto {len(data)} byte da {addr}")
-    print(f"Contenuto: {data}")
-
-
-except Exception as e:
-    print(f"Errore: {e}")
-
-finally:
-    sock.close()
-
-
-
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-try:
-    print(f"Invio della query DNS  tramite TCP   a {server_address[0]}:{server_address[1]}...")
-
-    # Connessione al server DNS (porta 53)
-    sock.connect(server_address)
-    sock.sendall(dns_message.encode())
-    # Ricevi i dati dal socket
-    respone = sock.recv(4096)  # Leggi fino a 4096 byte
-    print(f"Ricevuto: {respone.hex()}")
-
-
-except Exception as e:
-    print(f"Errore: {e}")
-
-finally:
-    sock.close()
-
-print("--------------")
-print(dns_message.encode())
+        sock.sendall(msg)
+        response = bytes()
+        while True:
+            rcvd = sock.recv(4096) 
+            print(rcvd)
+            response = response + rcvd
+            if len(rcvd) == 0:
+                break
+        print(f"Ricevuto: {response}")
+    except Exception as e:
+        print(f"Errore: {e}")
+    finally:
+        sock.close()
